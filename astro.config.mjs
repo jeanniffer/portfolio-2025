960706d077fc5b462e8dcfddf1e4988fd7f74cc7
Jeanniffer <jeannifferpimentel@gmail.com>
2025-04-04 12:19:16 -0500

    Workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,8 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://jeanniffer.github.io",
+  base: "portfolio-2025",
   vite: {
     plugins: [tailwindcss()],
   },
